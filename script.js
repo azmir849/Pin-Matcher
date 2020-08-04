f07d6fe366c645e06b252f01b301daafbd97bebd
azmir849 <azmir15-849@diu.edu.bd>
2020-08-04 20:19:40 +0600

    automatic pin generate button acitivity added

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 document.getElementById('match-pin').style.display = 'none';
 document.getElementById('misMatch-pin').style.display = 'none';
+
+//Automatic pin generate button activity
+document.getElementById("pin-generate-button").addEventListener("click", function() {
+    document.getElementById("pin-OTP").value = Math.floor(1000 + Math.random() * 8999);
+})
