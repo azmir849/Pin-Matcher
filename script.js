d83e912390e1fbcb46ebf01612a985f3cb57c243
azmir849 <azmir15-849@diu.edu.bd>
2020-08-04 20:41:30 +0600

    BackSpace Button Activity And final Code Added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById('match-pin').style.display = 'none';
-document.getElementById('misMatch-pin').style.display = 'none';
+    document.getElementById('match-pin').style.display = 'none';
+    document.getElementById('misMatch-pin').style.display = 'none';
 
 //Automatic pin generate button activity
-document.getElementById("pin-generate-button").addEventListener("click", function() {
+    document.getElementById("pin-generate-button").addEventListener("click", function() {
     document.getElementById("pin-OTP").value = Math.floor(1000 + Math.random() * 8999);
 })
 
