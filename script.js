3b0e552a679cb7ee80a07b10e0eaaebe5a9a0f9f
azmir849 <azmir15-849@diu.edu.bd>
2020-08-04 20:24:05 +0600

    Number buttons activity added

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ document.getElementById('misMatch-pin').style.display = 'none';
 document.getElementById("pin-generate-button").addEventListener("click", function() {
     document.getElementById("pin-OTP").value = Math.floor(1000 + Math.random() * 8999);
 })
+
+
+//Number Buttons(0-9) Activity
+function numberButton(num) {
+    document.getElementById('pin-entry').value = document.getElementById('pin-entry').value + num;
+}
